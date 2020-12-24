66776ff80778f892519be44043244024f4ffd85e
barthoekstra <barthoekstra@gmail.com>
2020-12-24 14:47:41 +0000

    Add mstop optimization script

---
+++ R/hyperparm_mboost_mstop.R
@@ -0,0 +1,14 @@
+library(mboost)
+library(tidyverse)
+
+data <- readRDS("data/processed/data.RDS")
+model <- readRDS("data/processed/models/mod_resids.RDS")
+outname <- "cvm_resids"
+
+# Determine and save cross-validation folds
+cv10f <- cv(model.weights(model), type = "kfold")
+saveRDS(cv10f, file = "data/processed/models/cv10f_resids.RDS")
+
+# Bootstrap optimal boosting iteration mstop
+cvm <- cvrisk(model, folds = cv10f, grid = seq(from = 250, to = 30000, by = 250))
+saveRDS(cvm, file = paste0("data/processed/models/", outname, ".RDS"))
