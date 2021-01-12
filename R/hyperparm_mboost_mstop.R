cbac269c5e2e0d7005c0c9faed50206f6b1151fe
barthoekstra <barthoekstra@gmail.com>
2021-01-12 18:54:14 +0000

    Parameterize model name

--- R/hyperparm_mboost_mstop.R
+++ R/hyperparm_mboost_mstop.R
@@ -1,13 +1,15 @@
 library(mboost)
 library(tidyverse)
 
-model <- readRDS("data/processed/models/mod_resids.RDS")
-outname <- "cvm_resids"
+model_name <- "trends"
+
+# Load model
+model <- readRDS(paste0("data/processed/models/mod_", model_name, ".RDS"))
 
 # Determine and save cross-validation folds
 cv10f <- cv(model.weights(model), type = "kfold")
-saveRDS(cv10f, file = "data/processed/models/cv10f_resids.RDS")
+saveRDS(cv10f, file = paste0("data/processed/models/cv10f_", model_name, ".RDS"))
 
 # Bootstrap optimal boosting iteration mstop
 cvm <- cvrisk(model, folds = cv10f, grid = seq(from = 250, to = 30000, by = 250))
-saveRDS(cvm, file = paste0("data/processed/models/", outname, ".RDS"))
+saveRDS(cvm, file = paste0("data/processed/models/cvm_", model_name, ".RDS"))
